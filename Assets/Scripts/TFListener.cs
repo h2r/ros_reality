6d6616898f2205f34899bbf344dd54288bc5ce37
David Whitney <david_whitney@brown.edu>
2017-09-27 16:48:47 -0400

    robot factory now parses pr2!

--- Assets/Scripts/TFListener.cs
+++ Assets/Scripts/TFListener.cs
@@ -7,7 +7,7 @@ public class TFListener : MonoBehaviour
 
 
 	private WebsocketClient wsc;
-	string topic = "ros_unity";
+	public string topic = "ros_unity";
 
 	public float scale = 1f;
 
@@ -15,20 +15,20 @@ public class TFListener : MonoBehaviour
 	void Start ()
 	{
         wsc = GameObject.Find("WebsocketClient").GetComponent<WebsocketClient>();
-		wsc.Subscribe (topic, "std_msgs/String", "none", 0);
+		wsc.Subscribe (topic, "std_msgs/String", 0);
 
 		//Attach table stuff
 		GameObject basePivot = GameObject.Find ("basePivot"); // replace with hashmap
 		GameObject table = GameObject.Find ("Table");
-		table.transform.SetParent (basePivot.transform);
-		table.transform.localPosition = new Vector3 (0f, -.15f, 0f);
-		table.transform.localScale = new Vector3 (0.2123f, 1f, 0.2123f);
+		table.transform.SetParent (basePivot.transform); //make table the parent object of the base pivot
+		table.transform.localPosition = new Vector3 (0f, -.15f, 0f); //new local position
+		table.transform.localScale = new Vector3 (0.2123f, 1f, 0.2123f);//local scaling of robot
 	}
 
 	void Update () 
 	{
-		string message = wsc.messages[topic];
-		string[] tfElements = message.Split (';');
+		string message = wsc.messages[topic]; //get newest robot state data (from transform)
+		string[] tfElements = message.Split (';'); //split the message into each joint/link data pair
         //Debug.Log(string.Join(", ", tfElements));
         foreach (string tfElement in tfElements) {
             //Debug.Log(tfElement);
@@ -37,20 +37,21 @@ public class TFListener : MonoBehaviour
 			GameObject cur = GameObject.Find (dataPair [0] + "Pivot"); // replace with hashmap
 			if (cur != null) {
 
-				string[] tmp = dataPair [1].Split ('^');
-				string pos = tmp [0];
-				string rot = tmp [1];
+				string[] tmp = dataPair [1].Split ('^'); //seperate position from rotation data
+				string pos = tmp [0]; //position data
+				string rot = tmp [1]; //rotation data
 				pos = pos.Substring (1, pos.Length - 2);
 				rot = rot.Substring (1, rot.Length - 2);
                 string[] poses = pos.Split (',');
-				float pos_x = float.Parse (poses [0]);
-				float pos_y = float.Parse (poses [1]);
-				float pos_z = float.Parse (poses [2]);
+				float pos_x = float.Parse (poses [0]); //x position
+				float pos_y = float.Parse (poses [1]); //y position
+				float pos_z = float.Parse (poses [2]); //z position
 
 
-                Vector3 curPos = new Vector3 (pos_x, pos_y, pos_z);
+                Vector3 curPos = new Vector3 (pos_x, pos_y, pos_z); //save current position
 				string[] rots = rot.Split (',');
-				float rot_x = float.Parse (rots [0]);
+                //save rotation as quaternions
+				float rot_x = float.Parse (rots [0]); 
 				float rot_y = float.Parse (rots [1]);
 				float rot_z = float.Parse (rots [2]);
 				float rot_w = float.Parse (rots [3]);
@@ -58,9 +59,9 @@ public class TFListener : MonoBehaviour
 
 				Quaternion curRot = new Quaternion (rot_x, rot_y, rot_z, rot_w);
 
-				cur.transform.position = Vector3.Lerp(scale * RosToUnityPositionAxisConversion (curPos), cur.transform.position, 0.7f);
-				cur.transform.rotation = Quaternion.Slerp(RosToUnityQuaternionConversion (curRot), cur.transform.rotation, 0.7f);
-				if (!cur.name.Contains("kinect")) {
+				cur.transform.position = Vector3.Lerp(scale * RosToUnityPositionAxisConversion (curPos), cur.transform.position, 0.7f); //convert ROS coordinates to Unity coordinates and scale for position vector
+				cur.transform.rotation = Quaternion.Slerp(RosToUnityQuaternionConversion (curRot), cur.transform.rotation, 0.7f); //convert ROS quaternions to Unity quarternions
+				if (!cur.name.Contains("kinect")) { //rescaling direction of kinect point cloud
 					cur.transform.localScale = new Vector3(scale, scale, scale);
 				} else {
 					cur.transform.localScale = new Vector3(-scale, scale, -scale);
@@ -71,11 +72,13 @@ public class TFListener : MonoBehaviour
 		}
 	}
 
+    //convert ROS position to Unity Position
 	Vector3 RosToUnityPositionAxisConversion (Vector3 rosIn)
 	{
 		return new Vector3 (-rosIn.x, rosIn.z, -rosIn.y);	
 	}
 
+    //Convert ROS quaternion to Unity Quaternion
 	Quaternion RosToUnityQuaternionConversion (Quaternion rosIn)
 	{
 		return new Quaternion (rosIn.x, -rosIn.z, rosIn.y, rosIn.w);

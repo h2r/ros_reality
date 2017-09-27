5468ee93ce629fa08543b65001e06c37ac4a3dfa
David Whitney <david_whitney@brown.edu>
2017-09-27 12:17:22 -0400

    added new scripts

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+        private WebsocketClient wsc;
+        string topic = "ros_unity";
+	// Use this for initialization
+	void Start () {
+        // Get the live websocket client
+
+        
+
+        wsc = GameObject.Find("WebsocketClient").GetComponent<WebsocketClient>();
+		wsc.Subscribe(topic, "std_msgs/String", "none", 0);
+
+       
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}

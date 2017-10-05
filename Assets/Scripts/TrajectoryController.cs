a9894762b9c06ac7cceeb5e65f47c9c434cfee20
David Whitney <david_whitney@brown.edu>
2017-10-05 13:24:31 -0400

    demonstration topic added

--- Assets/Scripts/TrajectoryController.cs
+++ Assets/Scripts/TrajectoryController.cs
@@ -5,19 +5,13 @@ using System.Threading;
 public class TrajectoryController : MonoBehaviour {
 
     public string arm;
-    public GameObject targetModel;
     Transform tf;
     private WebsocketClient wsc;
     private VRTK.VRTK_ControllerEvents controller;
     TFListener TFListener;
     float scale;
-    Vector3 lastControllerPosition;
-    Vector3 lastArmPosition;
-    Quaternion lastControllerRotation;
-    Quaternion lastArmRotation;
-    Transform lastArmTF;
-    Transform targetTransform;
     string message;
+    public GameObject followTarget;
 
     // Use this for initialization
     void Start() {
@@ -26,79 +20,39 @@ public class TrajectoryController : MonoBehaviour {
         wsc = wso.GetComponent<WebsocketClient>();
 
         wsc.Advertise("ein/" + arm + "/forth_commands", "std_msgs/String");
+        wsc.Advertise("/demonstrations", "std_msgs/String");
         controller = GetComponent<VRTK.VRTK_ControllerEvents>();
         TFListener = GameObject.Find("TFListener").GetComponent<TFListener>();
         tf = GetComponent<Transform>();
 
-        //last positions/rotation of the controller (calculate relative displacement of controller at each update)
-        lastControllerPosition = tf.position;
-        lastControllerRotation = tf.rotation;
-        Invoke("FindArm", 1f); //update position of lastArm position and rotation
-        InvokeRepeating("sendMessage", 1.2f, .1f); //send message to move arm by displacement of current controller position/rotation with previous position/rotation
-        targetTransform = targetModel.GetComponent<Transform>();
+        InvokeRepeating("myUpdate", 1.2f, .1f); //send message to move arm by displacement of current controller position/rotation with previous position/rotation
     }
 
-    void FindArm() { //update the lastArm with the current position/rotation of the controller
-        lastArmTF = GameObject.Find(arm + "_gripper_base").GetComponent<Transform>();
-        lastArmPosition = lastArmTF.position;
-        lastArmRotation = lastArmTF.rotation;
-        //Debug.Log(lastArmPosition);
-    }
 
-    void sendMessage() { //send an ein message to arm
-        wsc.SendEinMessage(message, arm);
-    }
 
-    void Update() {
+    void myUpdate() {
         scale = TFListener.scale;
-
-        Vector3 deltaPos = tf.position - lastControllerPosition; //displacement of current controller position to old controller position
-        lastControllerPosition = tf.position;
-
-        Quaternion deltaRot = tf.rotation * Quaternion.Inverse(lastControllerRotation); //delta of current controller rotation to old controller rotation
-        lastControllerRotation = tf.rotation;
 
         //message to be sent over ROs network
         message = "";
 
-
-        //Allows movement control with controllers if menu is disabled
-        if (controller.gripPressed) { //deadman switch being pressed
-            lastArmPosition = lastArmPosition + deltaPos; //new arm position
-            lastArmRotation = deltaRot * lastArmRotation; //new arm rotation
-
-            if ((Vector3.Distance(new Vector3(0f, 0f, 0f), lastArmPosition)) < 1.5) { //make sure that the trajectory 
-                targetTransform.position = lastArmPosition;
-            }
-            targetTransform.rotation = lastArmRotation;
-
-            //Vector3 outPos = UnityToRosPositionAxisConversion(lastArmTF.position + deltaPos) / scale;
-            Vector3 outPos = UnityToRosPositionAxisConversion(lastArmPosition) / scale;
-            //Quaternion outQuat = UnityToRosRotationAxisConversion(deltaRot * lastArmTF.rotation);
-            Quaternion outQuat = UnityToRosRotationAxisConversion(lastArmRotation);
-
-            message = outPos.x + " " + outPos.y + " " + outPos.z + " " + outQuat.x + " " + outQuat.y + " " + outQuat.z + " " + outQuat.w + " moveToEEPose";
-        }
-        else if (controller.touchpadPressed) {
-            float angle = controller.GetTouchpadAxisAngle();
-
-            if (angle >= 45 && angle < 135) // touching right
-                message += " yDown ";
-            else if (angle >= 135 && angle < 225) // touching bottom
-                message += " xDown ";
-            else if (angle >= 225 && angle < 315) // touching left
-                message += " yUp ";
-            else //touching top
-                message += " xUp ";
-        }
         if (controller.triggerPressed) {
-            message += " openGripper ";
+            followTarget.GetComponent<Renderer>().material.color = Color.green;
+
+            Vector3 outPos = UnityToRosPositionAxisConversion(followTarget.GetComponent<Transform>().position) / scale;
+
+            message = outPos.x + " " + outPos.y + " " + outPos.z + " " + 0 + " " + 1 + " " + 0 + " " + 0 + " moveToEEPose";
+            wsc.SendEinMessage(message, arm);
+
+            StartCoroutine(waiter());
+
+            wsc.Publish("/demonstrations", "REC");
         }
         else {
-            message += " closeGripper ";
+            followTarget.GetComponent<Renderer>().material.color = Color.red;
         }
 
-        //Debug.Log(message);
+        Debug.Log(message);
         //Debug.Log(lastArmPosition);
     }
 
@@ -110,6 +64,11 @@ public class TrajectoryController : MonoBehaviour {
         Quaternion temp = (new Quaternion(qIn.x, qIn.z, -qIn.y, qIn.w)) * (new Quaternion(0, 1, 0, 0));
         return temp;
     }
+
+    IEnumerator waiter() 
+        {
+            yield return new WaitForSeconds(.2f);
+        }
 
 }
 

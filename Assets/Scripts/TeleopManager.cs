78b415e16d7fc5710c61c31cd0b4313fa8a32b14
David Whitney <david_whitney@brown.edu>
2017-11-07 12:22:36 -0500

    adding players 1 and 2 for controls

---
+++ Assets/Scripts/TeleopManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleopManager : MonoBehaviour {
+    public int player_num = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

0ae874ff32bb8d7a3bcddd4caf250870fa1b67ee
David Whitney <david_whitney@brown.edu>
2017-11-08 14:05:57 -0500

    unique network id based names

---
+++ Assets/Scripts/PlayerID.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerID : NetworkBehaviour {
+
+    [SyncVar] public string playerUniqueIdentity;
+    private int playerNetID;
+    
+	public override void OnStartLocalPlayer() {
+        GetNetIdentity();
+        SetIdentity();
+    }
+
+    void Update() {
+        if (this.name == "" || this.name == "Player(clone)") {
+            SetIdentity();
+        }
+    }
+
+    [Client]
+    void GetNetIdentity() {
+        playerNetID = (int) GetComponent<NetworkIdentity>().netId.Value;
+        playerNetID /= 3;
+        CmdTellServerMyIdentity(MakeUniqueIdentity());
+    }
+
+    void SetIdentity() {
+        if(!isLocalPlayer) {
+            this.name = playerUniqueIdentity;
+        } else {
+            this.name = MakeUniqueIdentity();
+        }
+    }
+
+    string MakeUniqueIdentity() {
+        return "Player " + playerNetID.ToString();
+    }
+
+    [Command]
+    void CmdTellServerMyIdentity(string name) {
+        playerUniqueIdentity = name;
+    }
+}
